615e895df403d2bed35650a116b830c6a089a401
Mateusz Cywka <mateusz.cywka@relativity.com>
2018-06-14 09:43:34 +0200

    Add project files.

---
+++ MNTeamReportExporter/Program.cs
@@ -0,0 +1,49 @@
+﻿using LinqToExcel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNTeamReportExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = DateTime.Now.Date.AddDays(-1);
+
+            var excel = new ExcelQueryFactory(@"C:\Users\Mati\Downloads\MN_TEAM_FailedTransfersAnalysis.xlsx");
+
+            var transfers = (from c in excel.Worksheet<Transfer>("DATA")
+                             orderby c.Timestamp
+                             where c.Timestamp > date && c.Timestamp < date.AddHours(24)
+                            select c).ToList();
+
+            var groupedByTrasferType = transfers.GroupBy(x => x.TransferType, x=>x,
+                (key, g) => new { TransferType = key, Transfers = g.ToList() });
+
+            foreach(var item in groupedByTrasferType)
+            {
+                Console.WriteLine(item.TransferType);
+
+                var grouppedByTenant = item.Transfers.GroupBy(x => x.SourceName + " " + x.TenantNo, x=>x,
+                     (key, g) => new { Tenant = key, Transfers = g.ToList() });
+
+                foreach(var transfersForTenant in grouppedByTenant)
+                {
+                    Console.WriteLine(transfersForTenant.Tenant);
+
+                    foreach (var transfer in transfersForTenant.Transfers)
+                    {
+                        var line = $"{transfer.Timestamp} CorellationId: {transfer.CorellationId} Machine Name: {transfer.MachineName} Version: {transfer.Version} Reason: {transfer.Comment}";
+
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MNTeamReportExporter/Transfer.cs
@@ -0,0 +1,40 @@
+﻿using LinqToExcel.Attributes;
+using System;
+
+namespace MNTeamReportExporter
+{
+    public class Transfer
+    {
+        [ExcelColumn("Correlation ID")]
+        public string CorellationId { get; set; }
+
+        [ExcelColumn("Custom Data Transfer Type")]
+        public string TransferType { get; set; }
+
+        [ExcelColumn("Source Name")]
+        public string SourceName { get; set; }
+
+        [ExcelColumn("Timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [ExcelColumn("Custom Data Machine Name")]
+        public string MachineName { get; set; }
+
+        [ExcelColumn("Custom Data Version Number")]
+        public string Version { get; set; }
+
+        [ExcelColumn("Host Name")]
+        public string HostName { get; set; }
+
+        [ExcelColumn("Comment")]
+        public string Comment { get; set; }
+
+        public string TenantNo
+        {
+            get
+            {
+                return HostName.Replace("VM-","").Substring(0, 4);
+            }
+        }
+    }
+}
